634889c81a3a99d03d06089e5a7abb59769844e3
tomizdebski <tomizdebski@gmail.com>
2023-08-29 14:02:24 +0200

    updt1

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-// server/index.js
-
 const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors');
 
-app.use(cors()); // Add cors middleware
+app.use(cors()); 
 
 const server = http.createServer(app);
 
-// Add this
+
 app.get('/', (req, res) => {
     res.send('Hello world');
   });
